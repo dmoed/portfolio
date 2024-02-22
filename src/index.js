67cf7a60dfe85ef53d5673b56245111591188b1c
Don <dmoedjijo@gmail.com>
2024-02-22 10:59:21 -0300

    0.1.4

--- src/index.js
+++ src/index.js
@@ -17,6 +17,21 @@ function intro() {
     tl.to("#intro .animate-text", {y: 0, autoAlpha: 1, stagger: 0.15, ease: "power1.out"})
     tl.to("#intro .animate-c-text", {y: 0, autoAlpha: 1, stagger: 0.15, ease: "power1.out"}, "<=0.3")
     tl.to("#intro .animate-btn", {y: 0, autoAlpha: 1, stagger: 0.1, ease: "power1.out", duration: .6}, "-=0.3")
+
+    const tl2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#intro",
+            start: "top top",
+            end: "bottom top",
+            scrub: true
+        },
+    });
+
+    gsap.utils.toArray(".layer").forEach(layer => {
+        const depth = layer.dataset.depth;
+        const movement = -(layer.offsetHeight * depth)
+        tl2.to(layer, {y: movement, ease: "none"}, 0)
+    });
 }
 
 function section2() {
@@ -42,12 +57,15 @@ function section3() {
     });
 
     tl.set(["#section-3 .animate-text"], {y: "100%", autoAlpha: 0})
-    tl.to("#section-3 .animate-text", {y: 0, autoAlpha: 1, stagger: 0.1, ease: "power1.out"})
+    tl.to("#section-3 .animate-text", {y: 0, autoAlpha: 1, stagger: 0.1, ease: "power2.out", duration: .6})
 }
 
 document.addEventListener('DOMContentLoaded', function () {
 
     gsap.to("#default-loader", {y: "-100%", ease: "power1.out"})
+
+
+
 
     intro()
     section2()
